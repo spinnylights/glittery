449dc1cae878d1769ed51e4f554731eadd939451
Zack Anderson <zack@hush.es>
2013-02-08 19:45:41 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glittery::Application.config.secret_token = '60f9beb6b6345b670a87af02c95183e571c3dade08eb625036a0008a16a68b495d8ea2a9bb3663614254bb7ddfd69f69677e50050a7508bd0751cd3f6e410bba'
