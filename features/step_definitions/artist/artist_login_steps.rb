23cce7cb50d0c86c0cd65db59089013f5bf9060a
Zack Anderson <zack@hush.es>
2013-02-12 19:13:14 -0600

    write step definitions for artist_login.feature

---
+++ features/step_definitions/artist/artist_login_steps.rb
@@ -0,0 +1,27 @@
+Given /^that I am on the \/login page$/ do
+  visit '/login'
+end
+
+When /^I enter the correct credentials$/ do
+  fill_in 'Username', with: 'grangybears'
+  fill_in 'Password', with: 'iam2bearSk!n'
+  click_button 'Log in'
+end
+
+Then /^I should be on the main admin page$/ do
+  page.should have_content 'Administration'
+end
+
+When /^I enter the wrong credentials$/ do
+  fill_in 'Username', with: 'imabag'
+  fill_in 'Password', with: 'imabag'
+  click_button 'Log in'
+end
+
+Then /^I should still be on the \/login page$/ do
+  page.should have_content 'Log in'
+end
+
+Then /^I should be told that my credentials are wrong$/ do
+  page.should have_content 'Wrong username/password'
+end
