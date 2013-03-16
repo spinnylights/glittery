7cc6c15f24d6182ed2f6b0c39fde8be6df17978c
Zack Anderson <zack@hush.es>
2013-03-16 02:57:05 -0500

    refactor create_admin.rb

--- create_admin.rb
+++ create_admin.rb
@@ -1,12 +1,30 @@
 #!/user/bin/env ruby
+require 'rainbow'
 
-print "Username: "
-username = gets.chomp
-print "Password: "
-password = gets.chomp
+def auth_loop(attribute,length_minimum)
+  attr_hash = Hash.new
+  attr_hash[attribute] = ""
+
+  while true
+    print "#{attr_hash.keys.first.to_s.capitalize}: "
+    attr_hash[attribute] = gets.chomp
+    if attr_hash[attribute].size < length_minimum
+      puts ">> #{attr_hash[attribute].capitalize} must be at \
+least #{length_minimum} characters".bright
+    else
+      break
+    end
+  end
+  attr_hash
+end
+
+attributes = Hash.new
+attributes.merge!(auth_loop(:username, 6))
+attributes.merge!(auth_loop(:password, 8))
 
 File.open("config/admin.yml", "w") do |file|
   file.puts "---"
-  file.puts "!ruby/sym username: #{username}"
-  file.puts "!ruby/sym password: #{password}"
+  attributes.each do |key, value|
+    file.puts "!ruby/sym #{key}: #{value}"
+  end
 end
