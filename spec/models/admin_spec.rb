24612046f7aeb6cf4aaa024d7f8f81d9da4bd5fe
Zack Anderson <zack@hush.es>
2013-03-22 00:41:55 -0500

    authentication tests

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -12,12 +12,12 @@ describe Admin do
    @admin.should respond_to(:password_digest)
   end
 
-  describe "validations" do
+  describe 'validations' do
     it 'is valid with valid attributes' do
       @admin.should be_valid
     end
 
-    describe "for :username" do
+    describe 'for :username' do
       it 'is not valid without a username' do
         @admin.username = nil
         @admin.should_not be_valid
@@ -29,7 +29,7 @@ describe Admin do
       end
     end
 
-    describe "for :password" do
+    describe 'for :password' do
       it 'is not valid without a password' do
         @admin.password = nil
         @admin.should_not be_valid
@@ -56,13 +56,40 @@ describe Admin do
       end
 
       it 'is not valid with a password with forbidden characters' do
-        @admin.password = "^&*^&aA3%*"
+        @admin.password = '^&*^&aA3%*'
         @admin.should_not be_valid
       end
 
       it 'is not valid with a password under eight characters' do
         @admin.password = 'a' * 7
         @admin.should_not be_valid
+      end
+    end
+  end
+
+  describe 'authentication' do 
+    before do 
+      @admin.save 
+    end
+    
+    let(:admin_found_copy) { Admin.find_by_username(@admin.username) }
+
+    specify { @admin.should respond_to(:authenticate) }
+
+    context 'with valid password' do
+      specify 'authenticate should return self' do
+        @admin.should == admin_found_copy.authenticate(@admin.password)
+      end
+    end
+
+    context 'with invalid password' do
+      let(:invalid_password_admin) do 
+        admin_found_copy.authenticate("uggsbuggs")
+      end 
+
+      specify 'authenticate should return false' do
+        @admin.should_not == invalid_password_admin
+        invalid_password_admin.should be_false
       end
     end
   end
