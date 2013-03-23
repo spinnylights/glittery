bce4adfeb450e45ddcd8af178b2e48312a49c919
Zack Anderson <zack@hush.es>
2013-03-23 16:19:23 -0500

    get bio page working again

---
+++ features/step_definitions/appreciator_views_bio_steps.rb
@@ -0,0 +1,11 @@
+When /^I click on their bio link from the main page$/ do
+  visit '/'
+  click_link "bio"
+end
+
+Then /^I should see the artist's info/ do
+  page.should have_content artist_name
+  page.should have_selector(:xpath, ".//a[@href='mailto:#{artist_email}']")
+  page.should have_selector(:xpath, ".//img[@src='#{artist_photo_url}']")
+  page.should have_content artist_bio
+end
--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -6,3 +6,16 @@ Given /^I am logged in as "(.*?)" with the password "(.*?)"$/ do |username, pass
 
   store_admin(admin)
 end
+
+Given /^that an admin has an artist with info$/ do
+  admin = Admin.external_config_admin
+  artist = Admin.external_config_admin.artist
+
+  artist.update_attributes(name: artist_name,
+                           email: artist_email,
+                           photo_url: artist_photo_url,
+                           bio: artist_bio).should be_true
+  artist.name.should == artist_name
+
+  store_admin(admin)
+end
