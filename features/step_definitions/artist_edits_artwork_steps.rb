df3fd75bc5a26eda775f7f23b9866ac15c615756
Zack Anderson <zack@hush.es>
2013-03-24 03:20:37 -0500

    new artist_edits_artwork steps

--- features/step_definitions/artist_edits_artwork_steps.rb
+++ features/step_definitions/artist_edits_artwork_steps.rb
@@ -1,23 +1,16 @@
-When /^click edit under an artwork$/ do
-  page.find(:xpath, '//div[@id="Bag of Bags"]/
-            a[@href="/artworks/1/edit"]').click
+When /^I edit an artwork$/ do
+  visit '/'
+  page.find(".//div[@id='#{artwork_name}']//a[contains(./@href, 'edit')]").click
+  fill_in 'Name', with: new_artwork_name
+  fill_in 'Description', with: new_artwork_description
+  click_button 'Submit'
 end
 
-When /^change the artwork's name$/ do
-  fill_in 'Name', with: 'Whisper'
-end
-
-When /^change the artwork's description$/ do
-  fill_in 'Description', with: 'A quiet whisper.'
-end
-
-Then /^the artwork's name should be changed on the gallery and artwork pages$/ do
-  page.should have_content 'Whisper'
-  page.should_not have_content 'Bag of Bags'
-end
-
-Then /^the artwork's description should be changed on the artwork's page$/ do
-  find(:xpath, '//div[@class="image"]/a[@href="/artworks/1"]').click
-  page.should have_content 'Whisper'
-  page.should have_content 'A quiet whisper.'
+Then /^the artwork's info should be changed$/ do
+  visit '/'
+  page.should have_content new_artwork_name
+  page.should_not have_content artwork_name 
+  find(".//div[@id='#{new_artwork_name}']/div[@class='image']/a").click
+  page.should have_content new_artwork_name
+  page.should have_content new_artwork_description
 end
